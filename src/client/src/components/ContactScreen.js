b69faee5c5bf65a6b2498fb27c270c5f624530a3
Kian Akhavain <kakhavain55@gmail.com>
2022-06-18 10:39:10 -0700

    updated contactscreen.js

---
+++ src/client/src/components/ContactScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const ContactScreen = ({__
+
+}) => {
+  return (
+    <div class="container">
+        ContactScreen
+        <div class="item item-1"> </div>
+        <div class="item item-2"> </div>
+        <div class="item item-3"> </div>
+    </div>
+  );
+};
+
+export default ContactScreen;
